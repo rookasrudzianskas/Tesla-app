1c1660da618fe0f524b5c84e3831c6d806f79aa1
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 15:09:56 +0300

    Rokas

--- App.js
+++ App.js
@@ -5,17 +5,28 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69.000</Text>
+        </View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  carContainer: {
+  },
+  titles: {
+
+  },
+  title: {
+
+  },
+  subtitle: {
+
   },
 });
