045b7890d717ae9d7dedd5042d895d0eafa9bb55
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 17:23:00 +0300

    Rokas

---
+++ components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from "react-native";
+import styles from "./style";
+
+const Header = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default Header;
