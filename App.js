30903931d2c4a555b2f8a7cd5dc3f4babd9d1efc
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 16:33:02 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View, ImageBackground, FlatList} from 'react-native';
-import modelX from "./assets/images/ModelX.jpeg";
-import modelY from "./assets/images/ModelS.jpeg";
-import modelS from "./assets/images/ModelY.jpeg";
-import model3 from "./assets/images/Model3.jpeg";
-import CarItem from "./components/CarItem";
-import StyledButton from "./components/StyledButton";
+import CarsList from "./components/CarsList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList />
-      {/*<CarItem name={"Model Y"} tagline={"Starting from $105.000"} image={modelY}/>*/}
-      {/*<CarItem name={"Model S"} tagline={"Starting from $115.000"} image={modelS}/>*/}
-      {/*<CarItem name={"Model 3"} tagline={"Starting from $125.000"} image={model3}/>*/}
+      <StatusBar />
+      <CarsList />
     </View>
   );
 }
---
+++ components/CarsList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FlatList, Text, View} from "react-native";
+
+const CarsList = () => {
+    return (
+        <View>
+            <FlatList data={}  showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}/>
+        </View>
+    );
+};
+
+export default CarsList;
