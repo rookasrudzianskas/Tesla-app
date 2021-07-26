d0a04dd82faebe5ea452cad5302faa93c84e1343
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 15:16:17 +0300

    Roaks

--- App.js
+++ App.js
@@ -29,11 +29,15 @@ const styles = StyleSheet.create({
   },
   titles: {
     marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
   },
   title: {
-
+    fontSize: 40,
+    fontWeight: '500',
   },
   subtitle: {
-
+    fontSize: 16,
+    color: "#5c5e62",
   },
 });
