fe2bd33d081f2a9c6ca5e102d5c7eddb261c1d23
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 16:12:56 +0300

    Rokas

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -6,6 +6,9 @@ import styles from "./styles";
 import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
+
+    const {name, tagline, image} = props;
+
     return (
             <View style={styles.carContainer}>
                 <ImageBackground source={modelX} style={styles.image}>
@@ -15,8 +18,8 @@ const CarItem = (props) => {
                         <Text style={styles.subtitle}>Starting at $69.000</Text>
                     </View>
                     <View style={styles.buttonContainer}>
-                        <StyledButton type="primary" content={"Custom Order"} onPress={() => console.warn("Custom Order was pressed")} />
-                        <StyledButton type="secondary" content={"Existing Inventory"} onPress={() => console.warn("Existing Inventory was pressed")} />
+                        <StyledButton type="primary" content={"Custom Order"} onPress={() => console.log("")} />
+                        <StyledButton type="secondary" content={"Existing Inventory"} onPress={() => console.log("")} />
                     </View>
                 </ImageBackground>
             </View>
