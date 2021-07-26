3e88f739bd212b7dacd5590f11244fe2444f3e48
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 17:27:11 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View, ImageBackground, FlatList} from 'react-native';
 import CarsList from "./components/CarsList";
+import Header from "./components/Header";
 
 export default function App() {
 
   return (
     <View style={styles.container}>
+        <Header />
         <CarsList />
         <StatusBar style="auto" />
       {/*<CarItem name={cars.name} tagline={cars.tagline} image={cars.image}/>*/}
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View} from "react-native";
+import {Image, View} from "react-native";
 import styles from "./style";
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <View>
-
+        <View style={styles.container}>
+            <Image style={styles.logo} source={require('../../assets/images/logo.png')} />
+            <Image style={styles.menu} source={require('../../assets/images/menu.png')} />
         </View>
     );
 };
