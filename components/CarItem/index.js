ec083d48c9067c88369000abfff646912c58e085
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 16:54:47 +0300

    Rokas

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -11,17 +11,16 @@ const CarItem = (props) => {
 
     return (
             <View style={styles.carContainer}>
-                <ImageBackground source={image} style={styles.image}>
-                    <StatusBar style="auto" />
-                    <View style={styles.titles}>
-                        <Text style={styles.title}>{name}</Text>
-                        <Text style={styles.subtitle}>{tagline}<Text style={styles.subtitleCTA}>{" "}{taglineCTA}</Text></Text>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <StyledButton type="primary" content={"Custom Order"} onPress={() => console.log("")} />
-                        <StyledButton type="secondary" content={"Existing Inventory"} onPress={() => console.log("")} />
-                    </View>
-                </ImageBackground>
+                {/*<ImageBackground source={image} style={styles.image}>*/}
+                {/*    <View style={styles.titles}>*/}
+                {/*        <Text style={styles.title}>{name}</Text>*/}
+                {/*        <Text style={styles.subtitle}>{tagline}<Text style={styles.subtitleCTA}>{" "}{taglineCTA}</Text></Text>*/}
+                {/*    </View>*/}
+                {/*    <View style={styles.buttonContainer}>*/}
+                {/*        <StyledButton type="primary" content={"Custom Order"} onPress={() => console.log("")} />*/}
+                {/*        <StyledButton type="secondary" content={"Existing Inventory"} onPress={() => console.log("")} />*/}
+                {/*    </View>*/}
+                {/*</ImageBackground>*/}s
             </View>
     );
 };
