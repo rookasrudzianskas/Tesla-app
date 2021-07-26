cd93947ac39e4818df45dc6bf24c43c34130717f
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 15:59:46 +0300

    Rokas

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -7,11 +7,12 @@ const StyledButton = (props) => {
     const type = props.type;
 
     const backgroundColor = type === "primary" ? "black" : "white";
+    const textColor = type === "primary" ? "white" : "black";
 
     return (
         <View style={styles.container}>
             <Pressable style={[styles.button, {backgroundColor: backgroundColor}]} onPress={() => {console.log("Pressed")}}>
-                <Text style={styles.text}>Custom Order</Text>
+                <Text style={[styles.text, {color: textColor}]}>Custom Order</Text>
             </Pressable>
         </View>
     );
